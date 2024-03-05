bfbfceeb3a3637df591d7175a8f0377dcaaf62f8
Webster <strategywebstermanyanga@gmail.com>
2024-03-05 23:51:10 +0100

    Create Navigation Routing
    Added navigation links and stylings on navigation

--- Angular-App/src/app/nav/nav.component.ts
+++ Angular-App/src/app/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import {MatIconModule}from '@angular/material/icon'
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, RouterLink, RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss'
 })
--- Angular-App/src/app/tweets-page/tweets-page.component.ts
+++ Angular-App/src/app/tweets-page/tweets-page.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
 import { TweetComponent } from '../tweet/tweet.component';
 
 @Component({
