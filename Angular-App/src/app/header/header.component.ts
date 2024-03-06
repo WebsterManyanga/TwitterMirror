31518e4444edf303ad122cf2732265718d0f4d2b
Webster <strategywebstermanyanga@gmail.com>
2024-03-06 21:50:26 +0100

    Create Sidebar Menu
    Created a sidebar menu. Links and buttons are not yet functional

--- Angular-App/src/app/header/header.component.ts
+++ Angular-App/src/app/header/header.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatButtonModule, MatSidenavModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  showMenu = false;
 
+  toggleView() {
+    
+  }
 }
