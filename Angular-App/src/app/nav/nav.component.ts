b40e2327ca670d61781f167dd5d9497cd5359ced
Webster <strategywebstermanyanga@gmail.com>
2024-03-07 10:42:43 +0100

    Create Tweet Screen
    Created a template for the tweeting screen. Used text area for tweet area. Will check if there are better ways to develop this.

--- Angular-App/src/app/nav/nav.component.ts
+++ Angular-App/src/app/nav/nav.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
-import {MatIconModule}from '@angular/material/icon'
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-
+import { TweetingComponent } from '../tweeting/tweeting.component';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatIconModule, RouterLink, RouterLinkActive],
+  imports: [
+    MatIconModule,
+    RouterLink,
+    RouterLinkActive,
+    MatButtonModule,
+    MatDialogModule,
+  ],
   templateUrl: './nav.component.html',
-  styleUrl: './nav.component.scss'
+  styleUrl: './nav.component.scss',
 })
 export class NavComponent {
+  constructor(public dialog: MatDialog) {}
 
+  startTweet() {
+    const dialogRef = this.dialog.open(TweetingComponent, {
+      width: '100vw',
+      height: '100%',
+    });
+  }
 }
