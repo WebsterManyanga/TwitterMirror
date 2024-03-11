fe0d4d411ac8d3481783ad85958ad3f7e02561a9
Webster <strategywebstermanyanga@gmail.com>
2024-03-11 22:42:43 +0100

    Create Profile Page
    Started on profile page component. Added header.

--- Angular-App/src/app/profile/profile.component.ts
+++ Angular-App/src/app/profile/profile.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [MatIcon],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
