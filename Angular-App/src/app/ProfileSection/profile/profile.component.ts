7c13a34092df6436bf42f7ded7cba1bbb07c83a9
Webster <strategywebstermanyanga@gmail.com>
2024-03-24 22:19:51 +0100

    create lists

--- Angular-App/src/app/ProfileSection/profile/profile.component.ts
+++ Angular-App/src/app/ProfileSection/profile/profile.component.ts
@@ -3,11 +3,12 @@ import { MatIcon } from '@angular/material/icon';
 import { NavComponent } from '../nav/nav.component';
 import { UserService } from '../../user.service';
 import { User } from '../../user';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [MatIcon, NavComponent],
+  imports: [MatIcon, NavComponent, RouterLink],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss',
 })
--- Angular-App/src/app/app.routes.ts
+++ Angular-App/src/app/app.routes.ts
@@ -2,11 +2,16 @@ import { Routes } from '@angular/router';
 import { SettingsComponent } from './settings/settings.component';
 import { ProfileComponent } from './ProfileSection/profile/profile.component';
 import { HomeComponent } from './home/home.component';
+import { ListsComponent } from './lists/lists.component';
 
 export const routes: Routes = [
   {
     component: HomeComponent,
     path: ''
+  },
+  {
+    component: ListsComponent,
+    path: 'lists'
   },
   {
     component: SettingsComponent,
