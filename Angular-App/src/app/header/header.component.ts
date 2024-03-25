146d7ebdf39ca58f93df7adff29272d7cda31195
Webster <strategywebstermanyanga@gmail.com>
2024-03-25 13:46:59 +0100

    checkpoint

--- Angular-App/src/app/header/header.component.ts
+++ Angular-App/src/app/header/header.component.ts
@@ -1,22 +1,21 @@
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterLink } from '@angular/router';
 import { UserService } from '../user.service';
 import { User } from '../user';
-
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [MatIconModule, MatButtonModule, MatSidenavModule, RouterLink],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrl: './header.component.scss',
 })
 export class HeaderComponent {
   showMenu = false;
-  user:User = {
+  user: User = {
     name: '',
     username: '',
     bio: '',
@@ -24,17 +23,16 @@ export class HeaderComponent {
     headerPhoto: '',
     created: '',
     following: [],
-    followers: []
-  }
+    followers: [],
+  };
 
   @Input() headerTitle = `<div class='w-10'><img
   src="../assets/Twitter Logo.png"
   class="w-full"
   alt="twitter logo"
-/></div>`
+/></div>`;
 
-  constructor(private userService:UserService) {
-    this.user = {...userService.userInfo}
+  constructor(private userService: UserService) {
+    this.user = { ...userService.userInfo };
   }
-
 }
--- Angular-App/src/app/settings/settings.component.ts
+++ Angular-App/src/app/settings/settings.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import { RouterLink } from '@angular/router';
 @Component({
   selector: 'app-settings',
   standalone: true,
-  imports: [MatSlideToggleModule],
+  imports: [MatSlideToggleModule, MatIconModule, MatMenuModule, RouterLink],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
 export class SettingsComponent {
-
+  
 }
